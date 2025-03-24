994a8f7a753732542b2f92e9f1e822dab7e69475
Rohinigundala2019 <gundalarohini2019@gmail.com>
2025-03-24 19:49:05 +0530

    commiting the terraform file

--- Main.tf
+++ Main.tf
@@ -31,14 +31,14 @@ resource "aws_security_group" "Project-SG" {
 
 
 resource "aws_instance" "web" {
-  ami                    = "ami-001f2488b35ca8aad"
+  ami                    = "ami-04f7a54071e74f488"
   instance_type          = "t2.large"
-  key_name               = "Kastro"
+  key_name               = "Rohini"
   vpc_security_group_ids = [aws_security_group.Project-SG.id]
   user_data              = templatefile("./resource.sh", {})
 
   tags = {
-    Name = "Kastro"
+    Name = "Rohini"
   }
   root_block_device {
     volume_size = 30
